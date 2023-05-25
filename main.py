81958c5b80d63aac47e5d7cb5a18d9bb0dcaa1ec
pepito <rodriguezgh@gmail.com>
2023-05-25 07:09:00 -0300

    Clase 7 Request y Response

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-from flask import Flask, request, make_response, redirect, render_template;
+from flask import Flask, request
 
-app=Flask(__name__,template_folder='templates')
-
+app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    user_ip = request.remote_addr
-
-    response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
-
-    return response
-
-
-@app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
 
-    return render_template('hello.html', user_ip=user_ip)
-
+    user_ip = request.remote_addr
+    return f'Hello World, tu ip es {user_ip}'
